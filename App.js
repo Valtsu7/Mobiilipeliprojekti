d63821d21de062acb3e83bdc348ea895cbc925db
Valtsu7 <n1kova00@students.oamk.fi>
2025-02-06 22:52:54 +0200

    joko nyt

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on yourorusbgiurbgourp!  ing going to kill yuo allll</Text>
+      <Text>Open up App.js to start working on yourorusbgiurbygyufctuctugourp!  ing going to kill yuo allll</Text>
       <StatusBar style="auto" />
     </View>
   );
